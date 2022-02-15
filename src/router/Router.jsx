92b76edeb0f19807d8e9dfa84692db78c8184de6
Daniel <dochoahenao@gmail.com>
2022-02-15 02:27:17 -0500

    Categories Page added

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -4,12 +4,14 @@ import App from '../App';
 
 // Elements
 import Landing from '../components/pages/Landing/Landing';
+import Categories from '../components/pages/Categories/Categories';
 
 function Router() {
   return (
     <Routes>
       <Route path="/" element={<App />}>
         <Route path="/" element={<Landing />} />
+        <Route path="/categories" element={<Categories />} />
       </Route>
     </Routes>
   );
